a3c50d832c9503972ef91bc4ee419f899715e9c0
beingeek <furqan.790@outlook.com>
2025-03-13 07:53:00 +0000

    recent

--- phamos_supplier_addon/overrides/sales_invoice.py
+++ phamos_supplier_addon/overrides/sales_invoice.py
@@ -1,6 +1,8 @@
 import json
 import frappe
+import erpnext
 from frappe.utils import cstr, flt, cint
+from frappe.model.meta import get_field_precision
 from phamos_supplier_addon.phamos_supplier_addon.doctype.remote_server_connector.remote_server_connector import RemoteServerConnector
 
 
@@ -9,17 +11,28 @@ def fetch_and_process_work_summary(from_date, to_date, amount):
     RSC = RemoteServerConnector('Remote Server Connector', 'Remote Server Connector')
     work_summary = RSC.fetch_work_summary(from_date, to_date)
 
-    total_hours = flt(sum([ project[1] for project in work_summary]))
+    # company_currency = erpnext.get_company_currency(company)
+
+    precision = frappe.db.get_single_value("System Settings", "float_precision")
+    # precision = get_field_precision(frappe.get_meta("Sales Invoice Item").get_field("debit"), company_currency)
+
+    # frappe.msgprint(cstr(work_summary))
+    work_summary = [[d[0], flt(d[1], precision)] for d in work_summary]
+    # frappe.msgprint(cstr(work_summary))
+
+    total_hours = sum([ project[1] for project in work_summary])
     if total_hours > 0:
         hourly_rate = cint(amount)/ total_hours
         projects_name = json.loads(frappe.db.get_single_value("Remote Server Connector", "projects_name") or "{}") or {}
         work_summary = [
             {
                 "project": project,
-                "hours": hours,
+                "hours": flt(hours, precision),
                 "project_bill": hours * hourly_rate,
                 "project_name": get_project_name(project, projects_name, RSC)
-            } for project, hours in work_summary if hours > 0 ]
+            } for project, hours in work_summary if hours > 0
+        ]
+        # frappe.msgprint(cstr(sum([d.get("project_bill") for d in work_summary])))
         
         frappe.db.set_single_value("Remote Server Connector", "projects_name", json.dumps(projects_name), update_modified=False)
     return {"work_summary": work_summary}
--- phamos_supplier_addon/phamos_supplier_addon/doctype/remote_server_connector/remote_server_connector.js
+++ phamos_supplier_addon/phamos_supplier_addon/doctype/remote_server_connector/remote_server_connector.js
@@ -3,6 +3,79 @@
 
 frappe.ui.form.on('Remote Server Connector', {
     refresh: function(frm){
+        frm.add_custom_button(
+            __('Fetch work summary'), 
+            function () {
+            frappe.prompt([
+                {
+                    label: __("Timesheet Filters"),
+                    fieldname: "sec_brk_xt6tg",
+                    fieldtype: "Section Break",
+                },
+                {
+                    label: __("From"),
+                    fieldname: "from_date",
+                    fieldtype: "Date",
+                    default: frappe.datetime.month_start(),
+                    reqd: 1,
+                },
+                {
+                    fieldname: "col_brk_xt6tg",
+                    fieldtype: "Column Break",		
+                },
+                {
+                    label: __("To"),
+                    fieldname: "to_date",
+                    fieldtype: "Date",
+                    default: frappe.datetime.month_end(),
+                    reqd: 1,
+                },
+                {
+                    fieldname: "col_brk_xt6tg1",
+                    fieldtype: "Column Break",		
+                },
+                {
+                    label: __("Amount to Distribute"),
+                    fieldname: "amt_to_distribute",
+                    fieldtype: "Currency",
+                    reqd: 1,
+                }
+            ],
+            (values) => {
 
+                frappe.call({
+                    method: "phamos_supplier_addon.overrides.sales_invoice.fetch_and_process_work_summary",
+                    args: {
+                        "from_date": values.from_date,
+                        "to_date": values.to_date,
+                        "amount": values.amt_to_distribute
+                    },
+                    freeze: true,
+                    callback: function (r) {
+                        if (r.message.work_summary.length > 0) {
+                            let work_summary = r.message.work_summary;
+                            work_summary.forEach(row => {
+                                console.log(row);
+                                // const item_row = frm.add_child("items");
+                                // item_row.item_code = service_item;
+                                // item_row.item_name = service_item + " " + row.project;
+                                // item_row.description = service_item + " " + row.project;
+                                // item_row.custom_phamos_project = row.project;
+                                // item_row.custom_phamos_project_name = row.project_name;
+                                // item_row.income_account = income_account;
+                                // item_row.rate = row.project_bill;
+                                // item_row.custom_billable_hours = row.hours;
+                                // item_row.qty = 1;
+                                // item_row.amount = row.hours;
+                                // item_row.uom = "Hour";
+                            });
+                        } else {
+                            frappe.throw(__("No Data Found!"))
+                        }
+                    },
+                })
+            }, __("Fetch work summary"), __("Fetch"));
+            }
+        );
     }
 });
